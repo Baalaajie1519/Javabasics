236d39d6da68c1401c18ed17a0be86bf3531d4ff
Baalaajie1519 <balajikrishnaaa@gmail.com>
2018-05-29 15:45:17 -0700

    String Concatenation

---
+++ src/com/basics/DatatTypeExample.java
@@ -0,0 +1,12 @@
+package com.basics;
+
+public class  DatatTypeExample{
+	public static void main(String args[])
+	{
+		String firstname="Balaji";
+		String secondname="Raj";
+		
+	System.out.println(firstname+secondname);
+	}
+}
+
