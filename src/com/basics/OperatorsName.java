a166020e53356c35247fe4f06feb520d720646a8
Baalaajie1519 <balajikrishnaaa@gmail.com>
2018-05-29 16:12:33 -0700

    Operator Equality

---
+++ src/com/basics/OperatorsName.java
@@ -0,0 +1,21 @@
+package com.basics;
+
+public class OperatorsName {
+	public static void main(String args[])
+	{
+		int count=40;
+		count++;
+		
+		
+		count--; 
+		int negcount=-20;
+		boolean isFalse = true;
+		
+		System.out.println(!isFalse);
+		
+		
+	}
+	
+	
+
+}
