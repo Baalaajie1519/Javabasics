3929bf667b5311cc1da6067d53b347cc5f818885
Baalaajie1519 <balajikrishnaaa@gmail.com>
2018-05-30 10:30:52 -0700

    Mystery.2 program

---
+++ src/com/basics/Mystery2.java
@@ -0,0 +1,15 @@
+package com.basics;
+
+public class Mystery2 {
+	public static void main( String[] args )
+	{
+	int count = 1;
+
+	while ( count <=5 )
+	{
+	 System.out.println( count % 2 == 1 ? "****" : "++++++++" );
+	 ++count;
+	 } // end while
+	 } 
+
+}
