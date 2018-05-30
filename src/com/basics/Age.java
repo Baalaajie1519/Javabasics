c1a7d85f2c34e6b0a47dd020a9c640150dfdc754
Baalaajie1519 <balajikrishnaaa@gmail.com>
2018-05-30 15:40:14 -0700

    While Error finding

---
+++ src/com/basics/Age.java
@@ -0,0 +1,21 @@
+package com.basics;
+
+
+public class Age {
+public static void main(String args[])
+{
+	int Age = 64;
+	
+	
+	if (Age>= 65 )
+	{
+	System.out.println( "Age is greater than or equal to 65" );
+	}
+	else
+	{
+	System.out.println( "Age is less than 65");
+
+	
+}
+}	
+}
---
+++ src/com/basics/Number.java
@@ -0,0 +1,14 @@
+package com.basics;
+
+public class Number {
+public static void main(String args[])
+{
+	 int n = 1;
+	 
+	while (n <= 10 )
+	{
+	System.out.println( n++ );
+
+}
+}
+}
