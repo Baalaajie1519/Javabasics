7fe0dc8899aa586c365450d2ad000db77946bb78
Baalaajie1519 <balajikrishnaaa@gmail.com>
2018-05-29 23:52:14 -0700

    Do while

---
+++ src/com/basics/Dowhile.java
@@ -0,0 +1,21 @@
+package com.basics;
+
+public class Dowhile {
+	public static void main(String args[])
+	{
+		int i =0;
+		while(i<10)
+		{
+		System.out.println("i=" +i);
+	i++;
+		
+		}
+		i=0;
+		do {
+			System.out.println("Do while i=" + i);
+			i++;
+			
+		} while(i  < 12);
+	}
+
+}
