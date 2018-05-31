39eff42748d3a948cd4ae8956baf601acef7ee79
Baalaajie1519 <balajikrishnaaa@gmail.com>
2018-05-31 15:11:52 +0530

    Date and Time

---
+++ src/com/basics/Date.java
@@ -0,0 +1,14 @@
+package com.basics;
+
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
+
+
+public class Date {
+	public static void main(String[] args) {    
+		   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		   LocalDateTime now = LocalDateTime.now();  
+		   System.out.println(dtf.format(now));  
+		  }    
+
+}
